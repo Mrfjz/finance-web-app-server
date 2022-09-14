30b87b9ca2b8dbcc72406e97df0727f4498ce568
mrfjz <chaosfjz@hotmail.com>
2022-09-14 19:32:52 +0800

    first commit

---
+++ src/main/java/mrfjz/application/financewebappserver/FinanceWebAppServerApplication.java
@@ -0,0 +1,13 @@
+package mrfjz.application.financewebappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceWebAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceWebAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mrfjz/application/financewebappserver/FinanceWebAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mrfjz.application.financewebappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceWebAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
